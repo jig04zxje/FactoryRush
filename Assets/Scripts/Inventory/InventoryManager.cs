e0b44e282eb7217a9a38723fee47a2a1099da40a
cuongnqhe173086 <cuongnqhe173086@fpt.edu.vn>
2026-03-04 13:52:37 +0700

    Add InventoryManager and ItemSO

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -1,16 +1,57 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class InventoryManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static InventoryManager Instance;
+
+    private Dictionary<ItemSO, int> inventory = new Dictionary<ItemSO, int>();
+
+    public UnityEvent OnInventoryChanged;
+
+    private void Awake()
     {
-        
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+
+        OnInventoryChanged = new UnityEvent();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Add item
+    public void Add(ItemSO item, int amount = 1)
     {
-        
+        if (inventory.ContainsKey(item))
+            inventory[item] += amount;
+        else
+            inventory[item] = amount;
+
+        OnInventoryChanged.Invoke();
+    }
+
+    // Remove item
+    public void Remove(ItemSO item, int amount = 1)
+    {
+        if (!inventory.ContainsKey(item)) return;
+
+        inventory[item] -= amount;
+
+        if (inventory[item] <= 0)
+            inventory.Remove(item);
+
+        OnInventoryChanged.Invoke();
+    }
+
+    // Check item
+    public bool Has(ItemSO item, int amount = 1)
+    {
+        return inventory.ContainsKey(item) && inventory[item] >= amount;
+    }
+
+    public Dictionary<ItemSO, int> GetInventory()
+    {
+        return inventory;
     }
 }
